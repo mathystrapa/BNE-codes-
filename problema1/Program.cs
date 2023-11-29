5fa11cf27fdfed13d20b1b2621fd9852be6d580d
mathystrapa <mathystrapa@gmail.com>
2023-11-29 17:55:50 -0300

    commit

---
+++ problema1/Program.cs
@@ -0,0 +1,22 @@
+﻿// PROBLEMA 1:	Crie uma Tabela que calcule pela fórmula o Rendimento de um Investimento
+
+using System;
+
+namespace InvestmentNamespace
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+
+    class Investment
+    {
+        public Investment()
+        {
+
+        }
+    }
+}
