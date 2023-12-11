de24f9a0b4c971257327c6c28bcf1852c3ee9706
mathystrapa <mathystrapa@gmail.com>
2023-12-11 17:58:05 -0300

    Problema 7 - Calculadora de investimento completa

---
+++ problema6/Program.cs
@@ -0,0 +1,119 @@
+﻿// Crie uma Tabela e Programa C# que leia valores da Entrada. Mostre os cálculos para um período de 8 meses e 10 dias e que mostre o Rendimento Futuro:
+// Entradas: 1000, 5500, 12000	
+// Taxas: 3 %, 2,48 % e 2 %
+// Período: 8 meses e 10 dias
+
+using System;
+
+namespace InvestmentWithDateTime
+{
+    class Program
+    {
+        static void Main()
+        {
+            double[] info = GetStartingCapitalAndInterestRate();
+            double startingCapital = info[0], monthlyInterestRate = info[1]/100;
+            DateTime todayDate = DateTime.Now;
+            DateTime eightMonthsAndTenDaysFromNow = todayDate.AddMonths(8).AddDays(10);
+            double daysOfDifference = Convert.ToDouble((eightMonthsAndTenDaysFromNow - todayDate).Days);
+            double dailyInterestRate = Investment.ConvertFromMonthlyToDaily(monthlyInterestRate);
+            Investment investment = new Investment(startingCapital, dailyInterestRate, daysOfDifference);
+
+            Console.WriteLine($"\nTaxa diária: {dailyInterestRate*100} %\n");
+
+            Console.WriteLine($"\nHoje é dia {todayDate.Day} do mês {todayDate.Month} do ano {todayDate.Year}.");
+            Console.WriteLine($"\nResultados do investimento de R$ {startingCapital} a uma taxa mensal de {monthlyInterestRate*100} %, começando agora, até o dia {eightMonthsAndTenDaysFromNow.Day} do mês {eightMonthsAndTenDaysFromNow.Month} do ano {eightMonthsAndTenDaysFromNow.Year} (daqui a 8 meses e 10 dias):\n\nMontante final: R$ {investment.EndingCapital.ToString("N2")}\nLucro líquido: R$ {investment.LiquidProfit.ToString("N2")}\nLucro percentual: {investment.PercentageProfit.ToString("N2")} %");
+
+            Console.ReadKey();
+        }
+
+        static double[] GetStartingCapitalAndInterestRate()
+        {
+            double[] starting_capital_and_interest_rate = new double[2];
+            string? user_input;
+
+            while (true)
+            {
+                Console.WriteLine("Insira o capital inicial: R$");
+                user_input = Console.ReadLine();
+                if (double.TryParse(user_input, out starting_capital_and_interest_rate[0]) == false){
+                    ErrorMessage();
+                }
+                else
+                {
+                    if (starting_capital_and_interest_rate[0] < 0)
+                    {
+                        ErrorMessage();
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nInsira a taxa mensal (em %): ");
+                        user_input = Console.ReadLine();
+                        if (double.TryParse(user_input, out starting_capital_and_interest_rate[1]) == false)
+                        {
+                            ErrorMessage();
+                        }
+                        else
+                        {
+                            if (starting_capital_and_interest_rate[1] < 0)
+                            {
+                                ErrorMessage();
+                            }
+                            else
+                            {
+                                return starting_capital_and_interest_rate;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        static void ErrorMessage()
+        {
+            Console.WriteLine("\nErro. Digite um valor válido.\n");
+        }
+    }
+
+    class Investment
+    {
+        public double StartingCapital, EndingCapital, LiquidProfit, PercentageProfit;
+
+        public Investment(double startingCapital, double interestRate, double period)
+        {
+            StartingCapital = startingCapital;
+            CalcInvestmentResults(interestRate, period);
+        }
+
+        private void CalcInvestmentResults(double interestRate, double period)
+        {
+            EndingCapital = StartingCapital * Math.Pow(1 + interestRate, period);
+            LiquidProfit = EndingCapital - StartingCapital;
+            PercentageProfit = LiquidProfit * 100 / StartingCapital;
+        }
+
+        public static double ConvertFromMonthlyToDaily(double monthly_interest_rate)
+        {
+            double dailyInterestRate = Math.Pow(monthly_interest_rate + 1, 1.0 / 30.0) - 1;
+            return dailyInterestRate;
+        }
+
+        public static double ConvertFromMonthlyToYearly(double monthly_interest_rate)
+        {
+            double yearlyInterestRate = Math.Pow(monthly_interest_rate + 1, 12) - 1;
+            return yearlyInterestRate;
+        } 
+
+        public static double ConvertFromYearlyToMonthly(double yearlyInterestRate)
+        {
+            double monthlyInterestRate = Math.Pow(yearlyInterestRate + 1, 1.0 / 12.0) - 1;
+            return monthlyInterestRate;
+        }
+
+        public static double ConvertFromDailyToMonthly(double dailyInterestRate)
+        {
+            double monthlyInterestRate = Math.Pow(dailyInterestRate + 1, 30) - 1;
+            return monthlyInterestRate;
+        }
+    }
+}
---
+++ problema7/Program.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Data;
+
+namespace problem7
+{
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Investment investment = new Investment();
+                Console.WriteLine("\nResultados finais do investimento:\n");
+                Console.WriteLine("| Data inicial |  Data final  | Capital inicial | Montante final | Lucro líquido | Lucro percentual | Total sacado |");
+                Console.WriteLine($"|   {investment.StartingDate.ToShortDateString()}   |  {investment.EndingDate.ToShortDateString()}  |  R$ {investment.StartingCapital.ToString("N2")}  |  R$ {investment.EndingCapital.ToString("N2")}  |   R$ {investment.LiquidProfit.ToString("N2")}   |    {Math.Round(investment.PercentageProfit, 3)} %    |  R$ {investment.TotalRescue.ToString("N2")}  |");
+
+                AskUser();
+            }
+        }
+
+        static void AskUser()
+        {
+            string? user_input;
+            while (true)
+            {
+                Console.WriteLine("\nDeseja fazer outra simulação de investimento? ('s' para sim e 'n' para não)");
+                user_input = Console.ReadLine();
+                if (string.IsNullOrEmpty(user_input) || (user_input.ToLower() != "s" && user_input.ToLower() != "n"))
+                {
+                    Console.WriteLine("\nErro. Digite uma resposta válida.\n");
+                }
+                else
+                {
+                    if (user_input.ToLower() == "n")
+                    {
+                        Environment.Exit(0);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    class Investment
+    {
+        public double StartingCapital, EndingCapital, LiquidProfit, PercentageProfit, MonthlyInterestRate, DailyInterestRate, TotalRescue = 0;
+        public DateTime StartingDate, EndingDate;
+
+        public Investment()
+        {
+            GetInfo();
+            DailyInterestRate = ConvertFromMonthlyToDaily(MonthlyInterestRate);
+            CalcMonthResult();
+        }
+
+        private double ConvertFromMonthlyToDaily(double monthlyInterestRate)
+        {
+            double dailyInterestRate = Math.Pow(monthlyInterestRate + 1, 1.0 / 30.0) - 1;
+            return dailyInterestRate;
+        }
+
+        private void CalcMonthResult()
+        {
+            double balance = StartingCapital, liquid_profit = 0, rescue = 0, previousBalance, daysForNextMonth;
+
+            for (DateTime date = StartingDate; date <= EndingDate; date = date.AddMonths(1))
+            {
+                if (date != StartingDate.AddMonths(1))
+                {
+                    Console.WriteLine("\n|    Data    |  Rendimento  |  Saque  |   Saldo   |");
+                }
+
+                Console.WriteLine($"|  {date.ToShortDateString()}  |  R$ {liquid_profit.ToString("N2")}  |  R$ {rescue.ToString("N2")}  | R$ {balance.ToString("N2")} |");
+                if (date != EndingDate)
+                {
+                    if (date != StartingDate)
+                    {
+                        rescue = AskRescue();
+                        TotalRescue += rescue;
+                    }
+
+                    previousBalance = balance;
+                    balance -= rescue;
+                    daysForNextMonth = Convert.ToDouble((date.AddMonths(1) - date).Days);
+                    liquid_profit = balance * (Math.Pow(1 + DailyInterestRate, daysForNextMonth) - 1);
+                    balance += liquid_profit;
+                }
+            }
+            EndingCapital = balance;
+            LiquidProfit = EndingCapital - StartingCapital;
+            PercentageProfit = LiquidProfit * 100 / StartingCapital;
+        }
+
+        private static double AskRescue()
+        {
+            string? user_input;
+            double rescue;
+
+            while (true)
+            {
+                Console.WriteLine("\nDeseja sacar ? Digite o valor do saque (R$) ou aperte 'enter' para continuar.");
+                user_input = Console.ReadLine();
+                if (string.IsNullOrEmpty(user_input))
+                {
+                    return 0.0;
+                }
+                else
+                {
+                    if (double.TryParse(user_input, out rescue) == false)
+                    {
+                        ErrorMessage(0);
+                    }
+                    else
+                    {
+                        if (rescue < 0)
+                        {
+                            ErrorMessage(-1);
+                        }
+                        else
+                        {
+                            return rescue;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void GetInfo()
+        {
+            Console.WriteLine("\nInformações do investimento:\n");
+            string? user_input;
+
+            while (true)
+            {
+                Console.WriteLine("\nInsira o capital inicial: R$");
+                user_input = Console.ReadLine();
+                if (double.TryParse(user_input, out StartingCapital) == false)
+                {
+                    ErrorMessage(0);
+                }
+                else
+                {
+                    if (StartingCapital < 0)
+                    {
+                        ErrorMessage(-1);
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nInsira a taxa de juros mensal em pontos percentuais (%):");
+                        user_input = Console.ReadLine();
+                        if (double.TryParse(user_input, out MonthlyInterestRate) == false)
+                        {
+                            ErrorMessage(0);
+                        }
+                        else
+                        {
+                            if (MonthlyInterestRate < 0) 
+                            {
+                                ErrorMessage(-1);
+                            }
+                            else
+                            {
+                                MonthlyInterestRate /= 100;
+                                Console.WriteLine("\nInsira a data inicial do investimento (MM-DD-AAAA):");
+                                user_input = Console.ReadLine();
+                                if (DateTime.TryParse(user_input, out StartingDate) == false)
+                                {
+                                    ErrorMessage(1);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("\nInsira a data final do investimento (MM-DD-AAAA):");
+                                    user_input = Console.ReadLine();
+                                    if (DateTime.TryParse(user_input, out EndingDate) == false)
+                                    {
+                                        ErrorMessage(1);
+                                    }
+                                    else
+                                    {
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void ErrorMessage(int codError)
+        {
+            if (codError == -1)
+            {
+                Console.WriteLine("\nErro. O valor deve ser maior que 0.\n");
+            }
+            else
+            {
+                if (codError == 0)
+                {
+                    Console.WriteLine("\nErro. Valor inválido.\n");
+                }
+                else
+                {
+                    if (codError == 1)
+                    {
+                        Console.WriteLine("\nErro. Data inválida.\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nErro. Digite uma resposta válida.\n");
+                    }
+                }
+            }
+        }
+    }
+}
